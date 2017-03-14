57edc00eb6c1c353f5c0276beb08542705adbf44
Mahitha Valluru <mahitha.valluru@gmail.com>
2017-03-14 19:30:27 -0400

    Added 2 methods and created tests for them
    Added two methods: 
    1. contentLength() that computes the length (number of characters) in an XML document
    2. hasTag(String) that determines whether this XML document has a tag with the given name

--- XML.java
+++ XML.java
@@ -2,12 +2,27 @@ import tester.*;
 
 // represents a list of documents
 interface ILoXMLFrag {
+  // computes the length (number of characters) in an 
+  // XML document. Tags and attributes have no contribution
+  // to this length
+  int contentLength();
+  // does this XML document have a tag with the given name?
+  boolean hasTag(String that);
   
 }
 
 // represents an empty list of documents
 class MtLoXMLFrag implements ILoXMLFrag {
-  
+  // computes the length (number of characters) in an 
+  // XML document. Tags and attributes have no contribution
+  // to this length
+  public int contentLength() {
+    return 0;
+  }
+  // does this XML document have a tag with the given name?
+  public boolean hasTag(String that){
+    return false;
+  }
 }
 
 // represents a list of documents with at least one XMLFrag
@@ -20,10 +35,26 @@ class ConsLoXMLFrag implements ILoXMLFrag {
     this.first = first;
     this.document = document;
   }
+  // computes the length (number of characters) in an 
+  // XML document. Tags and attributes have no contribution
+  // to this length
+  public int contentLength() {
+    return this.first.contentLengthHelp() + this.document.contentLength();
+  }
+  // does this XML document have a tag with the given name?
+  public boolean hasTag(String that){
+    return this.first.hasTagHelp(that) || this.document.hasTag(that);
+  }
 }
 
 // represents a document
 interface IXMLFrag {
+  // determines the length of the IXMLFrag, but
+  // does not include Tags or Attributes
+  int contentLengthHelp();
+  // does this IXMLFrag have a Tag with the same
+  // name as the string given?
+  boolean hasTagHelp(String that);
   
 }
 
@@ -33,6 +64,17 @@ class Plaintext implements IXMLFrag {
   
   Plaintext(String txt) {
     this.txt = txt;
+  }
+  
+  // determines the length of the IXMLFrag, but
+  // does not include Tags or Attributes
+  public int contentLengthHelp() {
+    return this.txt.length();
+  }
+  // does this IXMLFrag have a Tag with the same
+  // name as the string given?
+  public boolean hasTagHelp(String that) {
+    return false;
   }
 }
 
@@ -45,6 +87,17 @@ class Tagged implements IXMLFrag {
     this.tag = tag;
     this.content = content;
   }
+  
+  // determines the length of the IXMLFrag, but
+  // does not include Tags or Attributes
+  public int contentLengthHelp() {
+    return this.content.contentLength();
+  }
+  // does this IXMLFrag have a Tag with the same
+  // name as the string given?
+  public boolean hasTagHelp(String that) {
+    return this.tag.hasTagAssist(that) || this.content.hasTag(that);
+  }
 }
 
 // represents a Tag with a name and a List of Attributes
@@ -55,6 +108,11 @@ class Tag {
   Tag(String name, ILoAtt atts) {
     this.name = name;
     this.atts = atts;
+  }
+  
+  // does this Tag have the same name as the string given?
+  boolean hasTagAssist(String that) {
+    return this.name.equals(that);
   }
 }
 
@@ -107,38 +165,107 @@ class ExamplesXML {
       new ConsLoAtt(this.durationAtt, 
           new MtLoAtt())));
   
+  // examples of IXMLFrag
+  IXMLFrag plaintext = new Plaintext("I am XML!");
+  IXMLFrag tagged = new Tagged(this.italicTag, 
+      new MtLoXMLFrag());
+  IXMLFrag complex = new Tagged(this.yell2Tag, 
+      new ConsLoXMLFrag(new Plaintext("I am XML!"), 
+          new MtLoXMLFrag()));
+  
   // examples of XML
-  ILoXMLFrag first = new ConsLoXMLFrag(new Plaintext("I"),     // "I am XML!"
-      new ConsLoXMLFrag(new Plaintext("am"), 
+  ILoXMLFrag empty = new MtLoXMLFrag();                        // empty ILoXMLFrag
+  ILoXMLFrag xml1 = new ConsLoXMLFrag(new Plaintext("I "),     // "I am XML!"
+      new ConsLoXMLFrag(new Plaintext("am "), 
           new ConsLoXMLFrag(new Plaintext("XML"),
               new ConsLoXMLFrag(new Plaintext("!"), 
                   new MtLoXMLFrag()))));
-  ILoXMLFrag second = new ConsLoXMLFrag(new Plaintext("I"),   // "I am <yell>XML</yell>!"
-      new ConsLoXMLFrag(new Plaintext("am"), 
+  ILoXMLFrag xml2 = new ConsLoXMLFrag(new Plaintext("I "),    // "I am <yell>XML</yell>!"
+      new ConsLoXMLFrag(new Plaintext("am "), 
           new ConsLoXMLFrag(new Tagged(this.yellTag, new ConsLoXMLFrag(new Plaintext("XML"), new MtLoXMLFrag())), 
               new ConsLoXMLFrag(new Plaintext("!"), 
                   new MtLoXMLFrag()))));
-  ILoXMLFrag third = new ConsLoXMLFrag(new Plaintext("I"),    // "I am <yell><italic>X</italic>ML</yell>!"
-      new ConsLoXMLFrag(new Plaintext("am"),
+  ILoXMLFrag xml3 = new ConsLoXMLFrag(new Plaintext("I "),    // "I am <yell><italic>X</italic>ML</yell>!"
+      new ConsLoXMLFrag(new Plaintext("am "),
           new ConsLoXMLFrag(new Tagged(this.yellTag, 
               new ConsLoXMLFrag(new Tagged(this.italicTag, 
                   new ConsLoXMLFrag(new Plaintext("X"), new MtLoXMLFrag())), 
                   new ConsLoXMLFrag(new Plaintext("ML"), new MtLoXMLFrag()))), 
               new ConsLoXMLFrag(new Plaintext("!"), new MtLoXMLFrag()))));  
-  ILoXMLFrag fourth = new ConsLoXMLFrag(new Plaintext("I"),   // "I am <yell volume="30db"><italic>X</italic>ML</yell>!"
-      new ConsLoXMLFrag(new Plaintext("am"),
+  ILoXMLFrag xml4 = new ConsLoXMLFrag(new Plaintext("I "),   // "I am <yell volume="30db"><italic>X</italic>ML</yell>!"
+      new ConsLoXMLFrag(new Plaintext("am "),
           new ConsLoXMLFrag(new Tagged(this.yell2Tag,
               new ConsLoXMLFrag(new Tagged(this.italicTag,
                   new ConsLoXMLFrag(new Plaintext("X"), new MtLoXMLFrag())),
                   new ConsLoXMLFrag(new Plaintext("ML"), new MtLoXMLFrag()))), 
               new ConsLoXMLFrag(new Plaintext("!"), new MtLoXMLFrag()))));
-  ILoXMLFrag fifth = new ConsLoXMLFrag(new Plaintext("I"),   // "I am <yell volume="30db" duration="5sec"><italic>X</italic>ML</yell>!"
-      new ConsLoXMLFrag(new Plaintext("am"),
+  ILoXMLFrag xml5 = new ConsLoXMLFrag(new Plaintext("I "),   // "I am <yell volume="30db" duration="5sec"><italic>X</italic>ML</yell>!"
+      new ConsLoXMLFrag(new Plaintext("am "),
           new ConsLoXMLFrag(new Tagged(this.yell3Tag,
               new ConsLoXMLFrag(new Tagged(this.italicTag,
                   new ConsLoXMLFrag(new Plaintext("X"), new MtLoXMLFrag())), 
                   new ConsLoXMLFrag(new Plaintext("ML"), new MtLoXMLFrag()))),
               new ConsLoXMLFrag(new Plaintext("!"), new MtLoXMLFrag()))));
+  
+  
+  // tests the contentLengthHelp() method, a helper for contentLength()
+  void testContentLengthHelp(Tester t) {
+    t.checkExpect(this.plaintext.contentLengthHelp(), 9);
+    t.checkExpect(this.tagged.contentLengthHelp(), 0);
+    t.checkExpect(this.complex.contentLengthHelp(), 9);
+    t.checkExpect(this.plaintext.contentLengthHelp() == this.complex.contentLengthHelp(), true);
+  }
+  
+  // tests the contentLength() method
+  void testContentLength(Tester t) {
+    t.checkExpect(this.empty.contentLength(), 0);
+    t.checkExpect(this.xml1.contentLength(), 9);
+    t.checkExpect(this.xml2.contentLength(), 9);
+    t.checkExpect(this.xml3.contentLength(), 9);
+    t.checkExpect(this.xml4.contentLength(), 9);
+    t.checkExpect(this.xml4.contentLength(), 9);
+  }
+  
+  // tests the hasTagAssist(String) method, a helper for hasTagHelp(String)
+  void testHasTagAssist(Tester t) {
+    t.checkExpect(this.italicTag.hasTagAssist("italic"), true);
+    t.checkExpect(this.italicTag.hasTagAssist("yell"), false);
+    t.checkExpect(this.italicTag.hasTagAssist("hello"), false);
+    t.checkExpect(this.yellTag.hasTagAssist("yell"), true);
+    t.checkExpect(this.yellTag.hasTagAssist("italic"), false);
+    t.checkExpect(this.yellTag.hasTagAssist("world"), false);
+    t.checkExpect(this.yell2Tag.hasTagAssist("yell"), true);
+    t.checkExpect(this.yell2Tag.hasTagAssist("hello"), false);
+    t.checkExpect(this.yell3Tag.hasTagAssist("yell"), true);
+    t.checkExpect(this.yell3Tag.hasTagAssist("world"), false);
+  }
+  
+  // tests the hasTagHelp(String) method, a helper for hasTag(String)
+  void testHasTagHelp(Tester t) {
+    t.checkExpect(this.plaintext.hasTagHelp("italic"), false);
+    t.checkExpect(this.plaintext.hasTagHelp("yell"), false);
+    t.checkExpect(this.tagged.hasTagHelp("italic"), true);
+    t.checkExpect(this.tagged.hasTagHelp("yell"), false);
+    t.checkExpect(this.complex.hasTagHelp("italic"), false);
+    t.checkExpect(this.complex.hasTagHelp("yell"), true);
+  }
+  
+  // tests the hasTag(String) method
+  void testHasTag(Tester t) {
+    t.checkExpect(this.empty.hasTag("italic"), false);
+    t.checkExpect(this.empty.hasTag("yell"), false);
+    t.checkExpect(this.xml1.hasTag("italic"), false);
+    t.checkExpect(this.xml1.hasTag("yell"), false);
+    t.checkExpect(this.xml2.hasTag("italic"), false);
+    t.checkExpect(this.xml2.hasTag("yell"), true);
+    t.checkExpect(this.xml3.hasTag("italic"), true);
+    t.checkExpect(this.xml3.hasTag("yell"), true);
+    t.checkExpect(this.xml4.hasTag("italic"), true);
+    t.checkExpect(this.xml4.hasTag("yell"), true);
+    t.checkExpect(this.xml5.hasTag("italic"), true);
+    t.checkExpect(this.xml5.hasTag("yell"), true);
+  }
+  
   
   
 }
